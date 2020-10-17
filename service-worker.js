f79423842865fc7bd98d9f98ca8e9a8dcf5a601a
AjinkyaBapat <ajinkyabapat12@gmail.com>
2020-10-17 10:42:51 +0000

    Deploying to gh-pages from  @ 5e97042bcdf002f93c7842f0ff1673e5465e0d6b 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.14343094039e14254bf1fb5702dc212d.js"
+  "/Portfolio/precache-manifest.def170782eec2562477a655c5d8b5107.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
